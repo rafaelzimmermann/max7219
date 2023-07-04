497e2fc84aed06882d9debe6f66323318b66c2a8
Rafael Zimmermann <engzimmermann@gmail.com>
2023-07-04 18:58:30 +0200

    Download uf2 proper for pico W

--- scripts/deploy-uf2.sh
+++ scripts/deploy-uf2.sh
@@ -10,6 +10,6 @@ VERSION="8.1.0"
 rm -f /mnt/pico/adafruit-circuitpython-raspberry_pi_pico-en_US-${VERSION}.uf2
 
 wget \
-    "https://downloads.circuitpython.org/bin/raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-${VERSION}.uf2" \
+    "https://downloads.circuitpython.org/bin/raspberry_pi_pico_w/en_US/adafruit-circuitpython-raspberry_pi_pico_w-en_US-${VERSION}.uf2" \
     -O "/tmp/adafruit-circuitpython-raspberry_pi_pico-en_US-${VERSION}.uf2"
 mv "/tmp/adafruit-circuitpython-raspberry_pi_pico-en_US-${VERSION}.uf2" "/mnt/pico/adafruit-circuitpython-raspberry_pi_pico-en_US-${VERSION}.uf2"
