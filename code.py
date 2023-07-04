e5dd19637c0682771fdc7e99d2a24be0ff345bc8
Rafael Zimmermann <engzimmermann@gmail.com>
2023-07-04 18:59:25 +0200

    Add pylint and refactor code

--- code.py
+++ code.py
@@ -1,45 +1,64 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import busio
 import digitalio
+import wifi
+import socketpool
+
+from secrets import secrets
 from libs import matrices
 
 
-# You may need to change the chip select pin depending on your wiring
-spi =  busio.SPI(clock=board.GP18, MOSI=board.GP19)
-cs = digitalio.DigitalInOut(board.GP17)
-matrix = matrices.CustomMatrix(width=32, height=8, spi=spi, cs=cs)
-while True:
-    print("Cycle start")
-    # all lit up
-    matrix.fill(True)
-    matrix.show()
-    time.sleep(0.5)
+class PyClock:
+    def __init__(self):
+        spi =  busio.SPI(clock=board.GP18, MOSI=board.GP19)
+        cs = digitalio.DigitalInOut(board.GP17)
+        self.matrix = matrices.CustomMatrix(width=32, height=8, spi=spi, cs=cs)
+        wifi.radio.connect(secrets.ssid, secrets.password)
+        self.pool = socketpool.SocketPool(wifi.radio)
 
-    # all off
-    matrix.fill(False)
-    matrix.show()
-    time.sleep(0.5)
+    def run(self):
+        self.blink()
+        while True:
+            self.update_clock()
 
-    # one column of leds lit
-    for i in range(8):
-        matrix.pixel(1, i, 1)
-    matrix.show()
-    time.sleep(0.5)
-    # now scroll the column to the right
-    for j in range(32):
-        matrix.scroll(1, 0)
-        matrix.show()
-        time.sleep(0.1)
+    def to_cest(self, h):
+        cest_h = int(h) + 2
+        if cest_h > 23:
+            cest_h = cest_h - 24
+        return str(cest_h)
 
-    adafruit = "test"
+    def update_clock(self):
+        try:
+            h, m, s = self.now().split(' ')[2].split(':')
+            h = self.to_cest(h)
+            self.matrix.fill(0)
+            self.matrix.text(f"{h}:{m}", 1, 1)
+            self.matrix.show()
+            time.sleep(60 - int(s))
+        except Exception as e:
+            print(e)
+            time.sleep(30)
 
-    # # scroll a string across the display
-    for pixel_position in range(len(adafruit) * 8):
-        matrix.fill(0)
-        matrix.text(adafruit, -pixel_position, 0)
-        matrix.show()
-        time.sleep(0.25)
+    def fill_and_show(self, value):
+        self.matrix.fill(value)
+        self.matrix.show()
+        time.sleep(0.5)
+
+    def blink(self):
+        self.fill_and_show(True)
+        self.fill_and_show(False)
+
+    def now(self):
+        address = ("time.nist.gov", 13)
+        s = self.pool.socket()
+        s.connect(address)
+        result = bytearray(51)
+        s.recv_into(result)
+        s.close()
+        return result.decode()
+
+
+if __name__ == "__main__":
+    clock = PyClock()
+    clock.run()
